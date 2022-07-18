bbce0b53666bf5a606d3614803132e16d55fc774
PouyaBarari <b_pooya@yahoo.com>
2022-07-18 10:57:47 +0430

    change css to module

--- src/view/NotFound/NotFound.js
+++ src/view/NotFound/NotFound.js
@@ -1,15 +1,15 @@
 import Panel from '../Dashboard/Panel';
-import './NotFound.scss';
+import style from './NotFound.module.scss';
 
 const NotFound = () => {
   return (
     <>
       <Panel />
-      <div className='page-404'>
-        <div className='page-404'>
-          <div className='div-main'>
-            <div className='number'>404</div>
-            <div className='text'>
+      <div className={style.page_404}>
+        <div className={style.page_404}>
+          <div className={style.div_main}>
+            <div className={style.number}>404</div>
+            <div className={style.text}>
               <span>متاسفیم...</span>
               <br></br>صفحه مورد نظر یافت نشد
             </div>
